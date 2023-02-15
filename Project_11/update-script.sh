df39ac1de5e61945c34cef4feacd821f8bb4cfe3
Tran Hoang Khang <48288606+khangtictoc@users.noreply.github.com>
2023-02-15 15:35:54 +0700

    Update and rename 007 update-script.sh to update-script.sh

--- Project_11/update-script.sh
+++ Project_11/update-script.sh
@@ -4,10 +4,13 @@ apt -y update
 apt -y upgrade
 
 # A LITTLE TRICK
+
 # '/var/run/reboot-required' is a file in Linux systems that is used to indicate 
 # that a reboot is required in order to apply updates or complete certain configuration changes. 
+
 # If this file exists, it means that the system administrator needs to reboot the system in order to 
 # finish installing updates or apply other changes. 
+
 # This file is typically created by package management systems like 'apt' or 'yum' when they install updates that 
 # require a reboot to complete.
 
